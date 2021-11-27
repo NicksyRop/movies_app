83b381ef0c7ac9bd98f81e8e8d876fd6f8386c14
NicksyRop <nicksonkipkorir25@gmail.com>
2021-11-27 09:46:26 +0300

    fresh laravel install plus tailwind css

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Movie App</title>
+</head>
+<body>
+
+    <nav>Nav Goes Here</nav>
+
+    @yield('content')
+
+</body>
+</html>
